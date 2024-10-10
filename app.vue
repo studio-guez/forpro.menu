5db539f4fe0d01d9fd83d32bab3687b9ed250981
azertypow <nico@villa1203.ch>
2024-10-10 18:43:03 +0200

    feat: init foodCourt template

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="v-app" >
+        <NuxtLayout>
+            <NuxtPage/>
+        </NuxtLayout>
+
+<!--        <div style="position: fixed; z-index: -1; top: 0; left: 50%; transform: translate(-50%, 0); width: 21cm; height: 29.7cm; background: #d5d5d5"></div>-->
+
+    </div>
+</template>
+
+
+
+
+
+<script setup lang="ts">
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+</style>
