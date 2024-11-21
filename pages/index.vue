a6f27220a1a33c19d6841952883a2b9fd9579c7c
azertypow <nico@villa1203.ch>
2024-11-21 21:00:30 +0100

    feat: next week access

--- pages/index.vue
+++ pages/index.vue
@@ -68,20 +68,43 @@ import AppHeader from "../components/AppHeader.vue";
 import AppTextContentFoodLab from "../components/AppTextContentFoodLab.vue";
 import AppSvgFoodLab from "../components/AppSvgFoodLab.vue";
 import {scaleTransform} from "~/utils/scaleTransform";
+import {useRouter} from "#app";
 
 const xlsxContent: Ref<Row[] | null> = ref(null)
 
 const elementToScale: Ref<UnwrapRef<null | HTMLElement>> = ref(null)
 const elementForSize: Ref<UnwrapRef<null | HTMLElement>> = ref(null)
 
+function isEvenWeek(date: Date): boolean {
+    const startOfYear = new Date(date.getFullYear(), 0, 1); // Premier jour de l'année
+    const dayOfYear = Math.floor((date.getTime() - startOfYear.getTime()) / (1000 * 60 * 60 * 24)) + 1; // Numéro du jour dans l'année
+
+    // Numéro de la semaine selon ISO 8601
+    const weekNumber = Math.ceil((dayOfYear + startOfYear.getDay() - 1) / 7);
+
+    return weekNumber % 2 === 0; // true si paire, false sinon
+}
+
+
 onMounted(() => {
+
+    const dateRef = new Date()
+
+    if(useRouter().currentRoute.value.query.next) {
+        dateRef.setDate(dateRef.getDay() + 7)
+    }
+
     fetch('https://hosting.for-pro.ch/foodlab.xlsx')
         .then(response => response.blob())
-        .then(blob => readXlsxFile(blob))
+        .then(blob => readXlsxFile(blob, {
+            sheet: isEvenWeek(dateRef) ? 2 : 1
+        }))
         .then((rows) => {
             xlsxContent.value = rows
-            console.log( rows )
+        }).catch(() => {
+          xlsxContent.value = null
         })
+
     nextTick(() => {
         setPageScale()
         window.addEventListener('resize', setPageScale)
